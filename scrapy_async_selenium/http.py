261bce908fbccf1f471993780a443df0ddd42a89
vyvojer <vyvojer@gmail.com>
2018-10-24 02:01:03 +0200

    Add docsting

--- scrapy_async_selenium/http.py
+++ scrapy_async_selenium/http.py
@@ -9,6 +9,22 @@ class SeleniumRequest(Request):
                  driver_func_kwargs: dict = {},
                  driver=None,
                  keep_driver=False, *args, **kwargs):
+        """
+        SeleniumRequest constructor
+
+        You can send some function, that will be running after page loading.
+        The function must have one required parameter of the selenium.webdriver.remote.webdriver.WebDriver type.
+        Also you can send optional args and kwargs to the function
+
+        Args:
+            driver_func(callable): function, that will be running after page loading
+            driver_func_args(tuple): Tuple of args
+            driver_func_kwargs(dict): Dict of kwargs
+            driver(http.Driver): use kept driver (see keep_driver)
+            keep_driver(bool): don't unblock driver after using. Driver will be keeped as meta['driver']. Use with caution
+            *args:
+            **kwargs:
+        """
         self.is_selenium = True
         self.driver_func = driver_func
         self.driver_func_args = driver_func_args
