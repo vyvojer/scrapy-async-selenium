18e7f1e306b58bfcbb58dc702a7e10e858e629d0
vyvojer <vyvojer@gmail.com>
2018-10-24 02:09:42 +0200

    Change packages

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='scrapy-session-proxies',
-      version='0.0.1',
+      version='0.0.2',
       description='Scrapy async selenium middleware',
       url='https://github.com/vyvojer/scrapy-async-selenium',
       author='Alexey Londkevich',
       author_email='vyvojer@gmail.com',
       license='MIT',
-      packages=['scrapy_session_proxies'],
+      packages=['scrapy_async_selenium'],
       install_requires=['scrapy', 'selenium'],
       zip_safe=False)
